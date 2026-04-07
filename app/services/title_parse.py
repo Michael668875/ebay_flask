f1eaa59bb610600a43f27db2672f9d0b3552ded1
Michael668875 <159018843+Michael668875@users.noreply.github.com>
2026-04-08 03:51:03 +1000

    adjusting title parse

--- app/services/title_parse.py
+++ app/services/title_parse.py
@@ -8,14 +8,6 @@ from app.services.titles import titles
 
 app = create_app()
 
-
-
-#title_list = ["Lenovo ThinkPad E14 Gen 7 /14/ AMD Ryzen 7- 7735U/ 16 GB RAM /512 GB SSD/ Win11",
-#              "Lenovo ThinkPad E14 Gen 8 /14/ AMD Ryzen 7- 7735U/ 16 GB RAM /512 GB SSD/ Win11",
-#              "Lenovo ThinkPad E14 Gen 6 /14/ AMD Ryzen 7- 7735U/ 16 GB RAM /512 GB SSD/ Win11",              
-#              ]
-#
-
 title_list = titles
 
 def normalize_title(title):
@@ -42,38 +34,102 @@ def find_model_near_thinkpad(title, known_models):
     return None
 
 
-# use a regex pattern to find the model in the title and save it to db
-def find_model_and_insert(title, known_models):
+# use a regex pattern to find the model in the title
+def find_model_by_pattern(title):
     title = normalize_title(title)
 
-    match = re.search(r"\be14\s+gen\s+(\d+)\b", title)
+    gen_match = re.search(r"\b[a-z]+(\d+)[a-z]?\s+(gen|g)\s*(\d+)\b", title)
 
-    if match:
-        model_name = match.group(0).title()   # "E14 Gen 7"
+    yoga_match = re.search(r"\b([a-z]+(\d+)\s+yoga)(\s*(gen|g)\s*(\d+))?\b", title)
 
-        # return model_name
+    carbon_match = re.search(r"\b([a-z]+(\d+)\s+carbon)(\s*(gen|g)\s*(\d+))?\b", title)
 
-        if model_name not in known_models:
-            try:
-                existing = TempModel.query.filter_by(temp_name=model_name).first()
+    simple_match = re.search(r"\b(x1|x9|x13|x12|t|p|e|l|w|z|a|sl|11e)\s*-?\s*(\d{1,4}(?:\s*2-in-1)?[a-z]?)\b", title) # this needs fixing. 
 
-                if not existing:
-                    new_model = TempModel(
-                        temp_name=model_name
-                    )
-                    db.session.add(new_model)
-                    db.session.commit()
+    X1_match = re.search(r"\b(x1)(?:\s*2-in-1)?(\s*(gen|g)\s*(\d+))?\b", title)
 
-                    known_models.add(model_name)
-                    print(f"Added new model: {model_name}")
-                else:
-                    print(f"Already exists in TempModel: {model_name}")
+    legacy_match = re.search(r"\b([a-z]+\d{2,4}[a-z]?)\b", title)
 
-            except Exception as e:
-                print(f"Error inserting {model_name}: {e}")
+    """
+    issues:
+    X1 Carbon 1st Gen
+    X13 2-in-1 Gen 5
+    EDGE 16 Mobile Workstation
+    X1 Tablet Gen 3
+    X12 Detachable Core 
+    X1 2-in-1 Gen 10 
+    X1 Yoga 3rd Gen
+    X9-14 G1 Core Ultra 5
+    T16 P16s
+    X13 Gen 2a
+    L13 Core
+    X1
 
-        else:
-            print(f"Already exists: {model_name}")
+    E15  found this instead: Tp0117A
+
+    x1 2-in-1 gen 10 found w11
+
+    """
+
+    if gen_match :
+        model_name = gen_match.group(0).title()   # "E14 Gen 7"
+        print(model_name)
+        return model_name
+    
+    elif yoga_match:
+        model_name = yoga_match.group(0).title()
+        print(model_name)
+        return model_name
+    
+    elif carbon_match:
+        model_name = carbon_match.group(0).title()
+        print(model_name)
+        return model_name
+    
+    elif simple_match:
+        model_name = simple_match.group(0).title()
+        print(model_name)
+        return model_name
+    
+    elif X1_match:
+        model_name = X1_match.group(0).title()
+        print(model_name)
+        return model_name
+    
+    elif legacy_match:
+        model_name = legacy_match.group(0).title()
+        print(model_name)
+        return model_name
+    
+    else:
+        print("no match")
+        return None
+
+# insert newly discovered model names into temp table    
+def insert_temp_model(model_name, known_models): 
+
+    if model_name not in known_models:
+        try:
+            existing = TempModel.query.filter_by(temp_name=model_name).first()
+
+            if not existing:
+                new_model = TempModel(
+                    temp_name=model_name
+                )
+                db.session.add(new_model)
+                db.session.commit()
+
+                known_models.add(model_name)
+
+                print(f"Added new model: {model_name}")
+            else:
+                print(f"Already exists in TempModel: {model_name}")
+
+        except Exception as e:
+            print(f"Error inserting {model_name}: {e}")
+
+    else:
+        print(f"Already exists: {model_name}")
 
 
 
@@ -81,8 +137,8 @@ with app.app_context():
     known_models = {m.name for m in ThinkPadModel.query.all()}
 
     for title in title_list:
-        find_model_near_thinkpad(title, known_models)
-        #find_model_and_insert(title, known_models)
+        #find_model_near_thinkpad(title, known_models)
+        find_model_by_pattern(title)
         
 
 # Parse title for CPU
