4185d151482645ac41cfc8ab79613e39a1122674
Michael668875 <159018843+Michael668875@users.noreply.github.com>
2026-04-25 22:13:41 +1000

    title parsing ready

--- app/services/main_fetch.py
+++ app/services/main_fetch.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 import os
 import sys
-import asyncio
 from app import create_app
-from app.services.save_temp import save_temp_summaries, save_temp_details
+from app.services.save_temp import save_temp_summaries
 from app.services.pipeline import run_pipeline, truncate_temp_tables
-from app.services.fetch import new_listings, get_paginated_summaries, fetch_item_details_async
-from app.services.parse import insert_storage_type, normalize_specs_field, parse_all_models, blacklist
+from app.services.fetch import get_paginated_summaries
+from app.services.parse import blacklist
 from datetime import datetime
 import traceback
+import tempfile
 
 # -----------------------------
 # CONFIG
@@ -16,7 +16,7 @@ import traceback
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 LOG_DIR = os.path.join(PROJECT_ROOT, "logs")
 LOG_FILE = os.path.join(LOG_DIR, "main_fetch.log")
-LOCK_FILE = "/tmp/main_fetch.lock"
+LOCK_FILE = os.path.join(tempfile.gettempdir(), "main_fetch.lock")
 
 
 # Ensure logs directory exists
@@ -26,13 +26,13 @@ os.makedirs(LOG_DIR, exist_ok=True)
 # -----------------------------
 # Prevent overlapping runs
 # -----------------------------
-#if os.path.exists(LOCK_FILE):
-#    print("Another main_fetch.py job is already running. Exiting.")
-#    sys.exit(0)
+if os.path.exists(LOCK_FILE):
+    print("Another main_fetch.py job is already running. Exiting.")
+    sys.exit(0)
 
 # Create lock file
-#with open(LOCK_FILE, "w") as f:
-#    f.write("")
+with open(LOCK_FILE, "w") as f:
+    f.write("")
 
 # -----------------------------
 # Main job
@@ -43,35 +43,24 @@ def main():
     try:
         with app.app_context():
             # truncate old data
-            #truncate_temp_tables()
+            truncate_temp_tables()
             # Fetch summaries
-        #    items = get_paginated_summaries()
-        #    clean_items = blacklist(items)
-        #    save_temp_summaries(clean_items)
-        #    print(f"Fetched and saved {len(items)} summaries")
-
-         #   # Fetch new listings and details
-         #   listings = new_listings()
-         #   details = asyncio.run(fetch_item_details_async(listings))
-         #   save_temp_details(details)
-         #   print(f"Fetched and saved details for {len(details)} listings")
+            items = get_paginated_summaries()
+            clean_items = blacklist(items)
+            save_temp_summaries(clean_items)
+            print(f"Fetched and saved {len(items)} summaries")
 
             # Run pipeline and parsing
             run_pipeline()
-           # insert_storage_type()
-           # normalize_specs_field("raw_ram", "ram", "ram_processed")
-           # normalize_specs_field("raw_storage", "storage", "storage_processed")
-           # parse_all_models()
-           # print("Pipeline and parsing completed successfully")
 
     except Exception:
         print("Error occurred in main_fetch.py:")
         traceback.print_exc()
-    #finally:
-    #    # Remove lock file
-    #    if os.path.exists(LOCK_FILE):
-    #        os.remove(LOCK_FILE)
-    #    print("Finished main_fetch.py job")
+    finally:
+        # Remove lock file
+        if os.path.exists(LOCK_FILE):
+            os.remove(LOCK_FILE)
+        print("Finished main_fetch.py job")
 
 # -----------------------------
 # Entry point
